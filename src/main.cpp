81fc5f48e93cfc36005e0efe72681f4cca249a37
AlicornLunaa <alicornlunaa@gmail.com>
2024-03-10 03:51:50 -0500

    Placement grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
+#include <vector>
+
 #include "SFML/Graphics/Color.hpp"
 #include "SFML/Graphics/PrimitiveType.hpp"
+#include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
 #include "SFML/Graphics/VertexArray.hpp"
+#include "SFML/System/Vector2.hpp"
 #include "SFML/Window/VideoMode.hpp"
 #include "SFML/Window/Event.hpp"
 
 #define WIDTH 640
 #define HEIGHT 480
-#define CELL_SIZE 10
-#define VERTICIAL_VERTEX_COUNT 2*(WIDTH/(float)CELL_SIZE)
-#define HORIZONTAL_VERTEX_COUNT 2*(HEIGHT/(float)CELL_SIZE)
+#define CELL_SIZE 20
+#define VERTICIAL_VERTEX_COUNT 2*(WIDTH/CELL_SIZE)
+#define HORIZONTAL_VERTEX_COUNT 2*(HEIGHT/CELL_SIZE)
 
 const sf::Color gridColor = sf::Color(40, 40, 40, 200);
 
@@ -39,12 +43,41 @@ void generate_array(sf::VertexArray& va){
     }
 }
 
+sf::Vector2i get_selected_cell(sf::RenderWindow& window){
+    sf::Vector2f pos = (sf::Vector2f)sf::Mouse::getPosition(window);
+    pos /= (float)CELL_SIZE;
+    pos.x = (int)pos.x;
+    pos.y = (int)pos.y;
+    return (sf::Vector2i)pos;
+}
+
+void update_cursor(sf::RenderWindow& window, sf::RectangleShape& cursor){
+    // Get current mouse position and round to grid
+    sf::Vector2f pos = (sf::Vector2f)get_selected_cell(window);
+    pos *= (float)CELL_SIZE;
+    cursor.setPosition(pos);
+}
+
+void toggle_cell(bool cellGrid[WIDTH / CELL_SIZE][HEIGHT / CELL_SIZE], const sf::Vector2i& pos){
+    cellGrid[pos.x][pos.y] = !cellGrid[pos.x][pos.y];
+}
+
 int main(){
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life", sf::Style::Default);
     sf::Event ev;
 
     sf::VertexArray gridArray(sf::PrimitiveType::Lines, VERTICIAL_VERTEX_COUNT + HORIZONTAL_VERTEX_COUNT);
     generate_array(gridArray);
+
+    sf::RectangleShape gridCursor({ CELL_SIZE, CELL_SIZE });
+    gridCursor.setFillColor(gridColor);
+    gridCursor.setOutlineColor(sf::Color::Cyan);
+    gridCursor.setOutlineThickness(2);
+
+    sf::RectangleShape cellSprite({ CELL_SIZE, CELL_SIZE });
+    cellSprite.setFillColor(sf::Color::White);
+
+    bool cellGrid[WIDTH / CELL_SIZE][HEIGHT / CELL_SIZE];
 
     while(window.isOpen()){
         while(window.pollEvent(ev)){
@@ -53,13 +86,29 @@ int main(){
                     window.close();
                     break;
 
+                case sf::Event::MouseButtonReleased:
+                    toggle_cell(cellGrid, get_selected_cell(window));
+                    break;
+
                 default:
                     break;
             }
         }
 
+        update_cursor(window, gridCursor);
+
         window.clear();
         window.draw(gridArray);
+        window.draw(gridCursor);
+
+        for(int x = 0; x < WIDTH / CELL_SIZE; x++){
+            for(int y = 0; y < HEIGHT / CELL_SIZE; y++){
+                if(!cellGrid[x][y]) continue;
+                cellSprite.setPosition(x * CELL_SIZE, y * CELL_SIZE);
+                window.draw(cellSprite);
+            }
+        }
+
         window.display();
     }
 
